63ecfe9ccd95e086a29d74b4d25691d4345fcb27
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2025-05-09 09:10:15 -0400

    Commit sett 1-3 sims (.R, .csv)

---
+++ sett3_mar_givZ.R
@@ -0,0 +1,156 @@
+# Libraries and functions
+library(Rsurrogate)
+source("R.s.miss.R")
+
+# Reproducibility 
+## Random seed to be used for each simulation setting
+args = commandArgs(TRUE)
+## When running on the cluster, give each array a unique seed by using the array ID
+sim_seed = as.integer(args)
+## Be reproducible queens
+set.seed(sim_seed) 
+
+# Functions to generate data 
+gen.data = function(setting, n1, n0) {
+  s1 = g.1(n1)
+  y1 = f.cond.1(s1)
+  s0 = g.0(n0)
+  y0 = f.cond.0(s0)
+  return(data.frame("s1" = s1, "y1" = y1, "s0" = s0, "y0" = y0))
+}
+f.cond.1 = function(s.vector) {
+  eps1 = rnorm(length(s.vector),0,3)
+  y1 = 2+5*s.vector+1 + 1*s.vector + eps1
+  return(y1)		
+}
+f.cond.0 = function(s.vector) {
+  eps0 = rnorm(length(s.vector),0,3)
+  y0 = 2+5*s.vector+ eps0
+  return(y0)		
+}
+g.1 = function(n, alpha0=5) { return(rnorm(n, alpha0 + 1,2))}
+g.0 = function(n,alpha0=5) { return(rnorm(n, alpha0,1))}
+
+# Run simulations 
+## Set number of replications per array
+REPS = 100
+## Set sample sizes 
+n1 = 1000
+n0 = 1000
+## Initialize empty dataframe for results
+sim_res = data.frame(
+  r = 1:REPS, 
+  gs_nonparam_delta = NA, gs_nonparam_delta.s = NA, gs_nonparam_R.s = NA, 
+  gs_param_delta = NA, gs_param_delta.s = NA, gs_param_R.s = NA, 
+  cc_nonparam_delta = NA, cc_nonparam_delta.s = NA, cc_nonparam_R.s = NA, 
+  cc_param_delta = NA, cc_param_delta.s = NA, cc_param_R.s = NA, 
+  ipw_nonparam_delta = NA, ipw_nonparam_delta.s = NA, ipw_nonparam_R.s = NA, 
+  ipw_param_delta = NA, ipw_param_delta.s = NA, ipw_param_R.s = NA,
+  smle_param_delta = NA, smle_param_delta.s = NA, smle_param_R.s = NA,
+  mle_param_delta = NA, mle_param_delta.s = NA, mle_param_R.s = NA) 
+for (r in 1:REPS) {
+  # Generate data 
+  data = gen.data(n1=n1, n0=n0) 
+  
+  # Define vectors for outcomes/surrogates in untreated/treated 
+  s1 = data$s1
+  y1 = data$y1
+  s0 = data$s0
+  y0 = data$y0
+  
+  ##########################################################
+  #Estimates with complete data ############################
+  ##########################################################
+  ## Estimate R with nonparametric approach (gold standard)
+  Rnonparam = R.s.estimate(sone=s1, 
+                           szero=s0, 
+                           yone = y1, 
+                           yzero = y0, 
+                           type = "robust")
+  sim_res[r, c("gs_nonparam_delta", "gs_nonparam_delta.s", "gs_nonparam_R.s")] = with(Rnonparam, c(delta, delta.s, R.s))
+  
+  ## Estimate R with parametric approach (gold standard) ###
+  Rparam = R.s.estimate(sone=s1, 
+                        szero=s0, 
+                        yone = y1, 
+                        yzero = y0, 
+                        type = "model")
+  sim_res[r, c("gs_param_delta", "gs_param_delta.s", "gs_param_R.s")] = with(Rparam, c(delta, delta.s, R.s))
+  
+  # Simulate non-missingness indicators ###################
+  ## Under MAR, probability of missingness depends on Z (logistic regression)
+  m1 = rbinom(n = n1, size = 1, prob = 1 / (1 + exp(- 0.75)))
+  m0 = rbinom(n = n0, size = 1, prob = 1 / (1 + exp(0.75)))
+  s0[m0==0] = NA ### make them missing
+  s1[m1==0] = NA ### make them missing
+  
+  ##########################################################
+  #Estimates with incomplete data ##########################
+  ##########################################################
+  ## Estimate R with nonparametric approach (complete case)
+  Rnonparam_miss = R.s.estimate(sone = s1[m1==1], 
+                                szero = s0[m0==1], 
+                                yone = y1[m1==1], 
+                                yzero = y0[m0==1], 
+                                type = "robust")
+  sim_res[r, c("cc_nonparam_delta", "cc_nonparam_delta.s", "cc_nonparam_R.s")] = with(Rnonparam_miss, c(delta, delta.s, R.s))
+  
+  ## Estimate R with parametric approach (complete case)
+  Rparam_miss = R.s.estimate(sone = s1[m1==1], 
+                             szero = s0[m0==1], 
+                             yone = y1[m1==1], 
+                             yzero = y0[m0==1], 
+                             type = "model")
+  sim_res[r, c("cc_param_delta", "cc_param_delta.s", "cc_param_R.s")] = with(Rparam_miss, c(delta, delta.s, R.s))
+  
+  ## Calculate weights for IPW approaches
+  m = c(m1, m0)
+  y = c(y1, y0)
+  ipw_fit = glm(formula = m ~ y, 
+                family = "binomial")
+  w1 = 1 / (1 + exp(-(ipw_fit$coefficients[1] + ipw_fit$coefficients[2] * y1)))
+  w0 = 1 / (1 + exp(-(ipw_fit$coefficients[1] + ipw_fit$coefficients[2] * y0)))
+  
+  ## Estimate R with nonparametric approach (IPW)
+  Rnonparam_miss_ipw = R.s.miss(sone = s1, 
+                                szero = s0, 
+                                yone = y1, 
+                                yzero = y0, 
+                                type = "robust",
+                                wone = w1, 
+                                wzero = w0)
+  sim_res[r, c("ipw_nonparam_delta", "ipw_nonparam_delta.s", "ipw_nonparam_R.s")] = with(Rnonparam_miss_ipw, c(delta, delta.s, R.s))
+  
+  ## Estimate R with parametric approach (IPW)
+  Rparam_miss_ipw = R.s.miss(sone = s1, 
+                             szero = s0, 
+                             yone = y1, 
+                             yzero = y0, 
+                             type = "model",
+                             wone = w1, 
+                             wzero = w0)
+  sim_res[r, c("ipw_nonparam_delta", "ipw_nonparam_delta.s", "ipw_nonparam_R.s")] = with(Rparam_miss_ipw, c(delta, delta.s, R.s))
+  
+  ## Estimate R with parametric approach (SMLE)
+  Rparam_miss_smle = R.s.miss(sone = s1, 
+                              szero = s0,
+                              yone = y1,
+                              yzero = y0, 
+                              type = "model", 
+                              smle = TRUE) 
+  sim_res[r, c("smle_param_delta", "smle_param_delta.s", "smle_param_R.s")] = with(Rparam_miss_smle, c(delta, delta.s, R.s))
+  
+  ## Estimate R with parametric approach (MLE)
+  Rparam_miss_mle = R.s.miss(sone = s1, 
+                             szero = s0,
+                             yone = y1,
+                             yzero = y0, 
+                             type = "model", 
+                             smle = FALSE) 
+  sim_res[r, c("mle_param_delta", "mle_param_delta.s", "mle_param_R.s")] = with(Rparam_miss_mle, c(delta, delta.s, R.s))
+  
+  ## Save 
+  sim_res |> 
+    write.csv(paste0("sett3_mar_givZ/sett3_mar_givZ_seed", sim_seed, ".csv"), 
+              row.names = FALSE)
+}
