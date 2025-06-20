87469a89763a37120e8352a26b6c469db2fc942c
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2025-06-20 16:23:40 -0400

    Load dplyr to make tables

--- tables/sett1_mcar.R
+++ tables/sett1_mcar.R
@@ -1,5 +1,6 @@
 # Load libraries
 library(kableExtra) ## for pretty LaTex tables
+library(dplyr) ## for table making
 
 # Source table-making function from GitHub
 devtools::source_url("https://raw.githubusercontent.com/sarahlotspeich/missing_surrogates/refs/heads/main/tables/table_of_estimates.R")
