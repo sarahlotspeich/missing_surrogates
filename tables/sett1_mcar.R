c9f6c8eab257c1ffe304121374ccfc4e2bf88914
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2025-07-20 13:02:17 -0400

    Create combined sett1_mcar results (.R, .csv)

--- tables/sett1_mcar.R
+++ tables/sett1_mcar.R
@@ -6,10 +6,20 @@ library(dplyr) ## for table making
 devtools::source_url("https://raw.githubusercontent.com/sarahlotspeich/missing_surrogates/refs/heads/main/tables/table_of_estimates.R")
 
 # Read in simulation results from GitHub
+## Gold standard + complete case + IPW 
 p = paste0("https://raw.githubusercontent.com/sarahlotspeich/missing_surrogates/refs/heads/main/simulations/sett1_mcar/sett1_mcar_seed", 0:19, ".csv")
 sim_res = do.call(dplyr::bind_rows, 
                   lapply(X = paste0(p, list.files(p)), 
                          FUN = read.csv))
+## SMLE 
+p = paste0("https://raw.githubusercontent.com/sarahlotspeich/missing_surrogates/refs/heads/main/simulations/sett1_mcar_smle/sett1_mcar_seed", 20:39, ".csv")
+sim_res = sim_res |> 
+  dplyr::bind_cols(
+    do.call(dplyr::bind_rows, 
+            lapply(X = paste0(p, list.files(p)), 
+                         FUN = read.csv)) |> 
+      dplyr::select(dplyr::starts_with("smle"))
+  )
 
 # Make table 
 sim_res |> 
