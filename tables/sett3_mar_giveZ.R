c58557b1b811c9da71fc3ef969b9fefe772f7095
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2025-07-20 14:32:54 -0400

    Sett 3 tabs/figs

--- tables/sett3_mar_giveZ.R
+++ tables/sett3_mar_giveZ.R
@@ -5,10 +5,7 @@ library(kableExtra) ## for pretty LaTex tables
 devtools::source_url("https://raw.githubusercontent.com/sarahlotspeich/missing_surrogates/refs/heads/main/tables/table_of_estimates.R")
 
 # Read in simulation results from GitHub
-p = paste0("https://raw.githubusercontent.com/sarahlotspeich/missing_surrogates/refs/heads/main/simulations/sett3_mar_givZ/sett3_mar_givZ_seed", 0:19, ".csv")
-sim_res = do.call(dplyr::bind_rows, 
-                  lapply(X = paste0(p, list.files(p)), 
-                         FUN = read.csv)) 
+sim_res = read.csv("https://raw.githubusercontent.com/sarahlotspeich/missing_surrogates/refs/heads/main/simulations/sett3_mar_givZ.csv")
 
 # Make table 
 sim_res |> 
