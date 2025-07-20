b12d559695f96a48e3d79f5437a10ad6c4c0bab8
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2025-07-20 14:33:12 -0400

    Update combine sett2 res script

--- simulations/combine_sett2_mar_givY.R
+++ simulations/combine_sett2_mar_givY.R
@@ -1,10 +1,10 @@
 ## Gold standard + complete case + IPW 
-p = paste0("https://raw.githubusercontent.com/sarahlotspeich/missing_surrogates/refs/heads/main/simulations/sett3_mar_givZ/sett3_mar_givZ_seed", 0:19, ".csv")
+p = paste0("https://raw.githubusercontent.com/sarahlotspeich/missing_surrogates/refs/heads/main/simulations/sett2_mar_givY/sett2_mar_givY_seed", 0:19, ".csv")
 sim_res = do.call(dplyr::bind_rows, 
                   lapply(X = paste0(p, list.files(p)), 
                          FUN = read.csv))
 ## SMLE 
-p = paste0("https://raw.githubusercontent.com/sarahlotspeich/missing_surrogates/refs/heads/main/simulations/sett3_mar_givZ_smle/sett3_mar_givZ_seed", 20:39, ".csv")
+p = paste0("https://raw.githubusercontent.com/sarahlotspeich/missing_surrogates/refs/heads/main/simulations/sett2_mar_givY_smle/sett2_mar_givY_seed", 20:39, ".csv")
 sim_res = sim_res |> 
   dplyr::bind_cols(
     do.call(dplyr::bind_rows, 
@@ -15,5 +15,5 @@ sim_res = sim_res |>
 
 ## Save combined data 
 sim_res |> 
-  write.csv("simulations/sett3_mar_givZ.csv", 
+  write.csv("simulations/sett2_mar_givY.csv", 
             row.names = FALSE)
