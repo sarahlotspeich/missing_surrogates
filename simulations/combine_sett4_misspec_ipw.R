681a90c00fbc8c80da867ebbffce947e1ffa09cc
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2025-07-27 18:15:33 -0400

    Combine sett4 sim res

---
+++ simulations/combine_sett4_misspec_ipw.R
@@ -0,0 +1,19 @@
+## Gold standard + complete case + IPW 
+p = paste0("https://raw.githubusercontent.com/sarahlotspeich/missing_surrogates/refs/heads/main/simulations/sett4_misspec_ipw/sett4_misspec_ipw_seed", 0:19, ".csv")
+sim_res = do.call(dplyr::bind_rows, 
+                  lapply(X = paste0(p, list.files(p)), 
+                         FUN = read.csv))
+## SMLE 
+# p = paste0("https://raw.githubusercontent.com/sarahlotspeich/missing_surrogates/refs/heads/main/simulations/sett3_mar_givZ_smle/sett3_mar_givZ_seed", 20:39, ".csv")
+# sim_res = sim_res |> 
+#   dplyr::bind_cols(
+#     do.call(dplyr::bind_rows, 
+#             lapply(X = paste0(p, list.files(p)), 
+#                    FUN = read.csv)) |> 
+#       dplyr::select(dplyr::starts_with("smle"))
+#   )
+
+## Save combined data 
+sim_res |> 
+  write.csv("simulations/sett4_misspec_ipw.csv", 
+            row.names = FALSE)
