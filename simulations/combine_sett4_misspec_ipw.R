4b6f55b252800e9b3c0396cd548ce91eee3e807d
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2025-07-27 21:01:58 -0400

    Add SMLE results to sett4 (.R, .csv)

--- simulations/combine_sett4_misspec_ipw.R
+++ simulations/combine_sett4_misspec_ipw.R
@@ -3,15 +3,15 @@ p = paste0("https://raw.githubusercontent.com/sarahlotspeich/missing_surrogates/
 sim_res = do.call(dplyr::bind_rows, 
                   lapply(X = paste0(p, list.files(p)), 
                          FUN = read.csv))
-## SMLE 
-# p = paste0("https://raw.githubusercontent.com/sarahlotspeich/missing_surrogates/refs/heads/main/simulations/sett3_mar_givZ_smle/sett3_mar_givZ_seed", 20:39, ".csv")
-# sim_res = sim_res |> 
-#   dplyr::bind_cols(
-#     do.call(dplyr::bind_rows, 
-#             lapply(X = paste0(p, list.files(p)), 
-#                    FUN = read.csv)) |> 
-#       dplyr::select(dplyr::starts_with("smle"))
-#   )
+# SMLE
+p = paste0("https://raw.githubusercontent.com/sarahlotspeich/missing_surrogates/refs/heads/main/simulations/sett4_misspec_ipw_smle/sett4_misspec_ipw_seed", 20:39, ".csv")
+sim_res = sim_res |>
+  dplyr::bind_cols(
+    do.call(dplyr::bind_rows,
+            lapply(X = paste0(p, list.files(p)),
+                   FUN = read.csv)) |>
+      dplyr::select(dplyr::starts_with("smle"))
+  )
 
 ## Save combined data 
 sim_res |> 
