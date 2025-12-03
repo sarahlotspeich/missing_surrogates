b945052a4f30bd7f573ebc181cb4111487048862
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2025-12-02 19:04:16 -0500

    Create table script for non-overlap

---
+++ tables/sett5_non_overlap.R
@@ -0,0 +1,12 @@
+# Load libraries
+library(kableExtra) ## for pretty LaTex tables
+
+# Source table-making function from GitHub
+devtools::source_url("https://raw.githubusercontent.com/sarahlotspeich/missing_surrogates/refs/heads/main/tables/table_of_estimates.R")
+
+# Read in simulation results from GitHub
+sim_res = read.csv("https://raw.githubusercontent.com/sarahlotspeich/missing_surrogates/refs/heads/main/simulations/sett5_non_overlap.csv")
+
+# Make table 
+sim_res |> 
+  table_of_estimates()
