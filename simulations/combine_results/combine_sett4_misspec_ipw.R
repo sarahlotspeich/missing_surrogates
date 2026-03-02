c23497024b8fc745ad4541c851e4b0de3cada4d6
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2026-03-02 07:13:29 -0500

    Combine new misspec sims where O ~ Y

--- simulations/combine_results/combine_sett4_misspec_ipw.R
+++ simulations/combine_results/combine_sett4_misspec_ipw.R
@@ -4,8 +4,7 @@ sim_res = do.call(dplyr::bind_rows,
                   lapply(X = paste0(p, list.files(p)), 
                          FUN = read.csv))
 # SMLE
-#p = paste0("https://raw.githubusercontent.com/sarahlotspeich/missing_surrogates/refs/heads/main/simulations/sett4_misspec_ipw_smle/sett4_misspec_ipw_seed", 20:39, ".csv")
-p = paste0("~/Documents/missing_surrogates/simulations/sett4_misspec_ipw_smle/sett4_misspec_ipw_seed", 20:39, ".csv")
+p = paste0("https://raw.githubusercontent.com/sarahlotspeich/missing_surrogates/refs/heads/main/simulations/sett4_misspec_ipw_smle/sett4_misspec_ipw_seed", 20:39, ".csv")
 sim_res = sim_res |>
   dplyr::bind_cols(
     do.call(dplyr::bind_rows,
@@ -17,4 +16,24 @@ sim_res = sim_res |>
 ## Save combined data 
 sim_res |> 
   write.csv("simulations/sett4_misspec_ipw.csv", 
+            row.names = FALSE)
+
+## Gold standard + complete case + IPW 
+p = paste0("https://raw.githubusercontent.com/sarahlotspeich/missing_surrogates/refs/heads/main/simulations/sett4_misspec_ipw_overfit/sett4_misspec_ipw_overfit_seed", 0:19, ".csv")
+sim_res = do.call(dplyr::bind_rows, 
+                  lapply(X = paste0(p, list.files(p)), 
+                         FUN = read.csv))
+# SMLE (from Setting 2)
+p = paste0("https://raw.githubusercontent.com/sarahlotspeich/missing_surrogates/refs/heads/main/simulations/sett2_mar_givY/smle_sett2_mar_givY_seed", 20:39, ".csv")
+sim_res = sim_res |> 
+  dplyr::bind_cols(
+    do.call(dplyr::bind_rows, 
+            lapply(X = paste0(p, list.files(p)), 
+                   FUN = read.csv)) |> 
+      dplyr::select(dplyr::starts_with("smle"))
+  )
+
+## Save combined data 
+sim_res |> 
+  write.csv("simulations/sett4_misspec_ipw_overfit.csv", 
             row.names = FALSE)
