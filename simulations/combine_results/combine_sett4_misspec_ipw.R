028249d586556772a0c7e3588641a220f352709a
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2026-03-02 08:31:43 -0500

    Add results with Y + Z (no main effect) IPW fit

--- simulations/combine_results/combine_sett4_misspec_ipw.R
+++ simulations/combine_results/combine_sett4_misspec_ipw.R
@@ -4,7 +4,7 @@ sim_res = do.call(dplyr::bind_rows,
                   lapply(X = paste0(p, list.files(p)), 
                          FUN = read.csv))
 # SMLE
-p = paste0("https://raw.githubusercontent.com/sarahlotspeich/missing_surrogates/refs/heads/main/simulations/sett4_misspec_ipw_smle/sett4_misspec_ipw_seed", 20:39, ".csv")
+p = paste0("https://raw.githubusercontent.com/sarahlotspeich/missing_surrogates/refs/heads/main/simulations/sett4_misspec_ipw/smle_sett4_misspec_ipw_seed", 20:39, ".csv")
 sim_res = sim_res |>
   dplyr::bind_cols(
     do.call(dplyr::bind_rows,
