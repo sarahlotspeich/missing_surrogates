3e4780a0ef0dda3932fe371ac8f77785eaa1d0d4
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2026-03-02 19:42:21 -0500

    Script for flipped fig

--- figures/sett4_misspec_ipw.R
+++ figures/sett4_misspec_ipw.R
@@ -23,17 +23,17 @@ res_long = sim_res |>
                                     labels = c("PTE Estimator: Nonparametric",
                                                "PTE Estimator: Parametric")), 
                 method = factor(x = method, 
-                                levels = c("gs_nonparam", "cc_nonparam", "ipw_nonparam_Yonly", "ipw_nonparam_Zonly", "ipw_nonparam_YZ",
-                                           "gs_param", "cc_param", "ipw_param_Yonly", "ipw_param_Zonly", "ipw_param_YZ","smle_param"), 
-                                labels = c("Gold\nStandard", "Complete\nCase", "IPW\n(Y Only)", "IPW\n(Z Only)", "IPW\n(Y and Z)",
-                                           "Gold\nStandard",  "Complete\nCase", "IPW\n(Y Only)", "IPW\n(Z Only)", "IPW\n(Y and Z)", "SMLE")), 
+                                levels = c("gs_nonparam", "cc_nonparam", "ipw_nonparam_Yonly", "ipw_nonparam_Zonly", "ipw_nonparam_YZ_main", "ipw_nonparam_YZ",
+                                           "gs_param", "cc_param", "ipw_param_Yonly", "ipw_param_Zonly", "ipw_param_YZ_main", "ipw_param_YZ","smle_param"), 
+                                labels = c("Gold\nStandard", "Complete\nCase", "IPW\n(Y Only)", "IPW\n(Z Only)", "IPW\n(Y + Z)", "IPW\n(Y x Z)",
+                                           "Gold\nStandard",  "Complete\nCase", "IPW\n(Y Only)", "IPW\n(Z Only)", "IPW\n(Y + Z)", "IPW\n(Y x Z)", "SMLE")), 
                 true_model = factor(x = true_model, 
                                     levels = c("O ~ Y + Z + Y x Z", "O ~ Y"),
                                     labels = c(TeX("Missingness Mechanism: $O \\sim Y + Z + Y \\times Z$"), 
                                                TeX("Missingness Mechanism: $O \\sim Y$"))))
 
 # Make a boxplot 
-cols = c("#787ff6", "#ffbd59", "#8bdddb", "#ff99ff",  "#7dd5f6", "#ff914d") ## color palette
+cols = c("#787ff6", "#ffbd59", "#8bdddb", "#ff99ff",  "#7dd5f6", "#ff914d", "#7bc96f") ## color palette
 res_long |> 
   ggplot(aes(x = method, y = est, fill = method)) + 
   geom_hline(yintercept = 0.5, linetype = 2, color = "black") + 
@@ -51,4 +51,23 @@ res_long |>
         strip.background = element_rect(fill = "black"), 
         strip.text = element_text(color = "white"))
 ggsave(filename = "figures/sett4_misspec_ipw_boxplot.pdf", 
-       device = "pdf", width = 10, height = 8)
+       device = "pdf", width = 12, height = 10)
+res_long |> 
+  ggplot(aes(x = method, y = est, fill = method)) + 
+  geom_hline(yintercept = 0.5, linetype = 2, color = "black") + 
+  geom_boxplot() + 
+  xlab("Method") + 
+  ylab("Estimate") + 
+  facet_grid(rows = vars(true_model), 
+             cols = vars(parametric), 
+             scales = "free", 
+             labeller = labeller(parametric = label_value, 
+                                 true_model = label_parsed)) + 
+  scale_fill_manual(name = "Method:", values = cols, guide = "none") + 
+  theme_minimal(base_size = 16) + 
+  theme(legend.position = "top", 
+        strip.background = element_rect(fill = "black"), 
+        strip.text = element_text(color = "white")) + 
+  coord_flip()
+ggsave(filename = "figures/sett4_misspec_ipw_boxplot_flip.pdf", 
+       device = "pdf", width = 14, height = 9)
